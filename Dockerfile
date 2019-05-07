db7f7684343b2d6b2d33977d833bb5b1ff955d3a
Gregor Riepl <Gregor.Riepl@swisstxt.ch>
2019-05-07 11:18:36 +0200

    Switched to global log4j config to allow mounting a dedicated data volume

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ LABEL description="Debian-based ApacheDS container"
 RUN apt update -y && apt install -y apacheds
 
 COPY startup.sh /startup.sh
-COPY log4j.properties /usr/share/apacheds/instances/default/conf/log4j.properties
+COPY log4j.properties /etc/apacheds/log4j.properties
 
 EXPOSE 10389
 CMD ["/bin/sh", "/startup.sh"]
--- startup.sh
+++ startup.sh
@@ -5,11 +5,11 @@ set -e
 . /etc/default/apacheds
 
 exec ${JAVA_HOME}/bin/java ${JAVA_OPTS} \
-            -Dapacheds.controls="${ADS_CONTROLS}" \
-            -Dapacheds.extendedOperations="${ADS_EXTENDED_OPERATIONS}" \
-            -Dlog4j.configuration="file:${ADS_INSTANCES}/${ADS_INSTANCE}/conf/log4j.properties" \
-            -Dapacheds.log.dir="${ADS_INSTANCES}/${ADS_INSTANCE}/log" \
-            -cp "${ADS_HOME}/lib/*" \
-            org.apache.directory.server.UberjarMain \
-            "${ADS_INSTANCES}/${ADS_INSTANCE}/"
+	-Dapacheds.controls="${ADS_CONTROLS}" \
+	-Dapacheds.extendedOperations="${ADS_EXTENDED_OPERATIONS}" \
+	-Dlog4j.configuration="file:/etc/apacheds/log4j.properties" \
+	-Dapacheds.log.dir="${ADS_INSTANCES}/${ADS_INSTANCE}/log" \
+	-cp "${ADS_HOME}/lib/*" \
+	org.apache.directory.server.UberjarMain \
+	"${ADS_INSTANCES}/${ADS_INSTANCE}/"
 
